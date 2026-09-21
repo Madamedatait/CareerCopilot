0590a6a8f9b2a538a179f73e2161852a54a5a1ad
Madamedatait <bhlitdataservices@gmail.com>
2026-09-21 13:03:24 +0000

    modif

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,14 +1,18 @@
+import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-import os
+
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("Database URL found:", DATABASE_URL is not None)
+if not DATABASE_URL:
+    raise ValueError(
+        "DATABASE_URL is not configured."
+    )
 
-engine = create_engine(DATABASE_URL)
-
-with engine.connect() as connection:
-    print("✅ Connected to Supabase PostgreSQL")
+engine = create_engine(
+    DATABASE_URL
+)
